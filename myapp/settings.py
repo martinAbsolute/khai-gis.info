0cd64563e3ff3c02c6e88c7857557db1b3b3cbad
Martin Bahniuk <martin.bahniuk@sigma.software>
2023-04-10 18:07:01 +0300

    Purge secret key from config

--- myapp/settings.py
+++ myapp/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ylk_j)$+cnxgas!vr31jm3mi#rbwg^o0!&0r3t9e(ddm63)3mj'
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
